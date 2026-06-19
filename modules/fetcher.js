7ae4b2d21fed9b5581a6d8770ea84ac574d4f5ae
Sneha Suresh <techstack@knometrix.com>
2026-06-20 03:37:48 +0530

    commit

--- modules/fetcher.js
+++ modules/fetcher.js
@@ -6,7 +6,7 @@ const exa = new Exa(process.env.EXA_API_KEY);
 const fetchFromExa = async (promptText) => {
 
   const response = await exa.searchAndContents(promptText, {
-  numResults: 20,
+  numResults: 10,
   type: 'auto',
   category: 'news',
   startPublishedDate: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000).toISOString()
