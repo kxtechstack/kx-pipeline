cf97898b593f50369f53285d6d4414eae457a43c
Sneha Suresh <techstack@knometrix.com>
2026-07-16 10:17:48 +0530

    commit

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/admin/invite-user', async (req, res) => {
 
   try {
     const { data, error } = await supabaseClient.auth.admin.inviteUserByEmail(email, {
-      redirectTo: 'https://pwdcheck.sneha-9a1.workers.dev/',
+      redirectTo: 'https://mainpwdcheck.sneha-9a1.workers.dev/',
       data: { client_id: clientId }
     });
 
